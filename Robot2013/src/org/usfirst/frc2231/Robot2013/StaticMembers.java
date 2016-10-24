dea9dc28bcafa7cc1a89909d496a75f6f097a8f0
Saar Yogev <saaryogev@gmail.com>
2016-10-24 20:36:15 +0300

    Added project and 2016 files
    Added the project to the Github directory, and added all of the relevant
    commands from the OnyxTronix2016 repo(link below). **not all commands
    are operational**, test them before any usage.

---
+++ Robot2013/src/org/usfirst/frc2231/Robot2013/StaticMembers.java
@@ -0,0 +1,8 @@
+package org.usfirst.frc2231.Robot2013;
+
+public class StaticMembers {
+	public static boolean isEmergencyState = true;
+	public static final double DISTANCE_PER_PULSE = 0.5;
+	public static final double ABSOLUTE_TOLERANCE = 5;
+	public static final double PERCENT_TOLERANCE = 10;
+}
